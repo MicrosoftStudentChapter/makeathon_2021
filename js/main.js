a561dd298e29e58e5c787eacd6c99def5a08a872
destrex271 <destrex271@gmail.com>
2022-01-15 22:42:35 +0530

    Resolved TBA alignment issue

--- js/main.js
+++ js/main.js
@@ -6,9 +6,6 @@ fullDiv.classList.remove('d-none');
 if(width<1024){
     smallDiv.classList.remove('d-none');
     fullDiv.classList.add('d-none');
-}else{
-    smallDiv.classList.add('d-none');
-    fullDiv.classList.remove('d-none');
 }
 window.addEventListener('orientationchange',()=>{
     width = screen.width;
